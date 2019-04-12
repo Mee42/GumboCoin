d012f1411a299bf5bead300f5b887d5cbead1787
carson <carson42g@gmail.com>
2019-04-12 13:49:22 -0400

    Cleaning up so commiting

--- src/main/kotlin/systems/carson/Testing.kt
+++ src/main/kotlin/systems/carson/Testing.kt
@@ -1,48 +1,61 @@
 package systems.carson
 
+import org.apache.commons.codec.digest.DigestUtils
 import java.security.*
 import javax.crypto.Cipher
 import sun.misc.BASE64Encoder
 import java.nio.charset.Charset
+import kotlin.random.Random
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.KeyGenerator
+import javax.crypto.spec.SecretKeySpec
 
+
+//const val debug = true
+
+fun alice():Person{
+    val alice = Person("Alice")
+    return PublicPerson(alice.name,alice.publicKey)
+}
 
 fun main() {
-    val bob = Person("bob")
-    val alice = Person("alice")
+    val bob = Person("Bob")
+    val alice = alice()
 
-    val data = "This is some data!".toBytes()
 
-    val signature = bob.sign(data)
-
-    val verified = Person.verify(alice,signature,data)
-    println(verified)
-    println(bob)
+    val data = Random(100).nextBytes(ByteArray(10000) { -1 })
+    val encrypted = bob.encryptAES(data)
 
 
 }
 
-private infix fun <A,B,C> Pair<A,B>.and(value :C):Triple<A,B,C> = Triple(first,second,value)
 
-private fun String.toBytes():ByteArray = toByteArray(Charset.forName("UTF-8"))
+private fun ByteArray.hash() = DigestUtils.sha1Hex(this.contentToString())
+
+private fun String.toBytesUTF8():ByteArray = toByteArray(Charset.forName("UTF-8"))
 
 class Signature(val byteArray: ByteArray)
 
-class Person(val name :String){
-//    private val sig: java.security.Signature = java.security.Signature.getInstance("SHA1WithRSA")
+class PublicPerson(name :String,private val publicKeyIn :PublicKey) :Person(name){
+    override val keyPair: KeyPair
+        get() = KeyPair(publicKeyIn,null)
+}
 
+open class Person(val name :String) {
 
-    private val keyPair = buildKeyPair()
+    protected open val keyPair = buildKeyPair(DigestUtils.sha1(name)!!.contentHashCode().toLong())
 
     val publicKey :PublicKey
         get() = keyPair.public
     private val privateKey :PrivateKey
-        get() = keyPair.private
+        get() = keyPair.private ?: error("Current Person implementation doesn't support functions that use the private key")
 
     fun sign(data :ByteArray):Signature{
         sig.initSign(privateKey)
         sig.update(data)
         return Signature(sig.sign())
     }
+
     companion object {
         private val sig = java.security.Signature.getInstance("SHA1WithRSA")
 
@@ -55,28 +68,73 @@ class Person(val name :String){
                 verify(person.publicKey, signature, data)
     }
 
+    fun publicKeyString() :String = BASE64Encoder().encode(publicKey.encoded).replace("\n","")
+
     override fun toString(): String {
-        return "Person(name=$name, pubKey=${BASE64Encoder().encode(publicKey.encoded).substring(0,10)})"
+        return "Person(name=$name, finger=${fingerprint().substring(0,10)})"
+    }
+
+    fun fingerprint():String = DigestUtils.sha1Hex(publicKey.encoded)
+
+    /** This needs to be below 245 bytes */
+    fun encrypt(data :ByteArray): ByteArray {
+        val cipher = Cipher.getInstance("RSA")
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey)
+        return cipher.doFinal(data)
+    }
+
+    fun decrypt(privateKey: PrivateKey, encrypted: ByteArray): ByteArray {
+        val cipher = Cipher.getInstance("RSA")
+        cipher.init(Cipher.DECRYPT_MODE, publicKey)
+        return cipher.doFinal(encrypted)
+    }
+
+    fun decrypt(encrypted: ByteArray):ByteArray = decrypt(privateKey,encrypted)
+
+    /** This will encrypt over 245 bytes */
+    fun encryptAES(data :ByteArray, publicKey :PublicKey): EncryptedData {
+        val iv = ByteArray(16) { -1 }
+        SecureRandom.getInstanceStrong().nextBytes(iv)
+
+        val keyGen = KeyGenerator.getInstance("AES")
+        keyGen.init(128)
+        val secretKey = keyGen.generateKey()
+
+
+        val ivParameterSpec = IvParameterSpec(iv)
+        val aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        aesCipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec)
+
+        val final = aesCipher.doFinal(data)
+        return EncryptedData(iv,encrypt(secretKey.encoded),final)
+        //need to return encrypted secret key and the encrypted message
+    }
+
+    fun decryptAES(data :EncryptedData,privateKey :PrivateKey) :ByteArray{
+        val iv = data.iv
+        val ivParameterSpec = IvParameterSpec(iv)
+
+        val decryptedSecretKey = decrypt(privateKey,data.encryptedSecretKey)
+
+        val secretKey = SecretKeySpec(decryptedSecretKey,0, decryptedSecretKey.size,"AES")
+
+
+        val aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+        aesCipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec)
+
+        return aesCipher.doFinal(data.encrypedData)
     }
 }
 
-fun buildKeyPair(): KeyPair {
+class EncryptedData(val iv :ByteArray, val encryptedSecretKey :ByteArray,val encrypedData :ByteArray)
+
+
+private fun buildKeyPair(seed :Long): KeyPair {
+    val random = SecureRandom.getInstance("SHA1PRNG")
+    random.setSeed(seed)
     val keySize = 2048
     val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
-    keyPairGenerator.initialize(keySize)
+    keyPairGenerator.initialize(keySize,random)
     return keyPairGenerator.genKeyPair()
 }
 
-fun encrypt(privateKey: PrivateKey, message: String): ByteArray {
-    val cipher = Cipher.getInstance("RSA")
-    cipher.init(Cipher.ENCRYPT_MODE, privateKey)
-
-    return cipher.doFinal(message.toByteArray())
-}
-
-fun decrypt(publicKey: PublicKey, encrypted: ByteArray): ByteArray {
-    val cipher = Cipher.getInstance("RSA")
-    cipher.init(Cipher.DECRYPT_MODE, publicKey)
-
-    return cipher.doFinal(encrypted)
-}
